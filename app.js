c10736b337462daa98c96ebd6888a4601b267b55
Sree Gopal Saha <sreegopal0101@gmail.com>
2024-08-17 00:27:45 +0530

    set view engine

--- app.js
+++ app.js
@@ -3,13 +3,18 @@ const app = express();
 require('dotenv').config();
 const db = require('./configs/mongooseConnection');
 const cookieParser = require('cookie-parser');
+const path = require("path");
 const PORT = process.env.PORT || 3000;
 const userRouter = require('./routes/userRouter');
 const postRouter = require('./routes/postRouter');
+const staticPath = path.join(__dirname, "public");
+const isUserLoggedIn = require("./middlewares/isUserLoggedIn");
 
 app.use(express.urlencoded({extended: true}));
 app.use(express.json());
 app.use(cookieParser());
+app.use(express.static(staticPath));
+app.set('view engine', 'ejs');
 
 app.get('/', (req, res)=>{
     res.send("Hello there!");
