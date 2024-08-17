226660bc66dadf38edebf9118fce59be66456041
Sree Gopal Saha <sreegopal0101@gmail.com>
2024-08-17 20:33:51 +0530

    modify feed.ejs for uploading imgage

--- public/javasrcripts/feed.js
+++ public/javasrcripts/feed.js
@@ -1,3 +1,6 @@
+const imageUploader = document.querySelector("#imageUploader");
+const postImgInput = document.querySelector("#postImgInput");
+
 document.addEventListener("DOMContentLoaded", async () => {
     const res = await fetch("http://localhost:3000/api/getFeed", {
         method: 'GET',
@@ -6,3 +9,20 @@ document.addEventListener("DOMContentLoaded", async () => {
     const data = await res.json();
     console.log(data);
 })
+
+imageUploader.addEventListener("click", ()=>{
+    postImgInput.click();
+});
+
+postImgInput.addEventListener("change", () => {
+    const file = postImgInput.files[0];
+    if (file) {
+        const reader = new FileReader();
+        reader.onload = function(e) {
+            imagePreview.src = e.target.result;
+            imagePreview.classList.remove("hidden");
+        }
+        reader.readAsDataURL(file);
+    }
+});
+
