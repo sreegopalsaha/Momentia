95230322785512e443eba2a757aa8183533ed799
Sree Gopal Saha <sreegopal0101@gmail.com>
2024-08-17 21:06:54 +0530

    add multer config

--- routes/postRouter.js
+++ routes/postRouter.js
@@ -1,7 +1,8 @@
 const exprees = require('express');
 const router = exprees.Router();
 const {newPost} = require('../controllers/postController');
+const upload = require("../configs/multerConfig");
 
-router.post('/new', newPost);
+router.post('/new', upload.single("image"), newPost);
 
 module.exports = router;
