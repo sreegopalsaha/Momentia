c06edc16a1c413c99d0b064d1da74a6f7fceec1c
Sree Gopal Saha <sreegopal0101@gmail.com>
2024-08-17 21:35:10 +0530

    modify postRouter for new post creation

--- controllers/postController.js
+++ controllers/postController.js
@@ -1,5 +1,22 @@
-const newPost = (req, res) =>{
-    res.send("i am working too!");
+const userModel = require("../models/user.model");
+const postModel = require("../models/post.model");
+
+const newPost = async(req, res) =>{
+    const user = await userModel.findById(req.user.id);
+    const author = user._id;
+    const {content} = req.body;
+    const image = req.file.filename;
+    // create the post
+    const post = await postModel.create({
+        author,
+        content,
+        image
+    });
+
+    // push the post id to user.posts
+    user.posts.push(post._id);
+    await user.save();
+    return res.redirect("/feed");
 }
 
 module.exports = {newPost};
--- routes/postRouter.js
+++ routes/postRouter.js
@@ -2,7 +2,8 @@ const exprees = require('express');
 const router = exprees.Router();
 const {newPost} = require('../controllers/postController');
 const upload = require("../configs/multerConfig");
+const {isUserLoggedIn} = require("../middlewares/isLoggedIn");
 
-router.post('/new', upload.single("image"), newPost);
+router.post('/new', isUserLoggedIn, upload.single("image"), newPost);
 
 module.exports = router;
