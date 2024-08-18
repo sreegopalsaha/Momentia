64cb1fbe14d6359544aced94e7cb3f3016b7ba17
Sree Gopal Saha <sreegopal0101@gmail.com>
2024-08-18 10:46:49 +0530

    modify feed.js to append posts on allPostContainer

--- api/getFeed.js
+++ api/getFeed.js
@@ -4,14 +4,17 @@ const userModel = require("../models/user.model");
 const getFeed = async (req, res) => {
 
     const user = await userModel.findById(req.user.id).populate('following');
+    if(user.following.length<1 && user.posts.length<1 ){
+        return res.json("");
+    }
     const followingUserIds = user.following.map(followingUser => followingUser._id);
     followingUserIds.push(user._id);
     const followingPosts = await postModel.find({ author: { $in: followingUserIds } })
-    .populate("author", "fullname username")
+    .populate("author", "fullname username profilePicture")
     .sort({ createdAt: -1 })
     .limit(10);
 
-     res.json(followingPosts);
+    res.json(followingPosts);
 
 }
 
--- public/javasrcripts/feed.js
+++ public/javasrcripts/feed.js
@@ -8,64 +8,73 @@ document.addEventListener("DOMContentLoaded", async () => {
         credentials: 'include'
     });
     const data = await res.json();
+
     allPostsContainer.innerHTML = "";
 
-    data.forEach(post => {
-        const postContainer = document.createElement("div");
-        postContainer.classList.add("postContainer", "border-2", "border-zinc-200", "rounded-lg", "w-full", "h-fit", "p-4");
-        
-        postContainer.innerHTML = `
-        <div class="userDetails flex items-center gap-1 h-12">
-            <div class="profilePicDiv w-10 h-10 rounded-full overflow-hidden">
-                <img class="w-10"
-                    src="https://media.licdn.com/dms/image/D4D03AQGG79xYxGe2uQ/profile-displayphoto-shrink_200_200/0/1694942905209?e=2147483647&v=beta&t=I5P_2oMiMWQcEk_A2KMTobYaZkrwhAyzKVWBs4cZcvc"
-                    alt="">
-            </div>
+    if (!data) {
+        allPostsContainer.innerHTML = `
+        <div class="w-full h-[20rem] flex flex-col gap-5 items-center justify-center p-5">
+        <p>Oops! No posts here yet. Start following more awesome people to fill your feed with exciting content.</p>
+        <button class="w-[10rem] h-10 bg-blue-500 rounded-lg">View Suggestions</button>
+    </div>`
+    } else {
+        data.forEach(post => {
+            const postContainer = document.createElement("div");
+            postContainer.classList.add("postContainer", "border-2", "border-zinc-200", "rounded-lg", "w-full", "h-fit", "p-4");
 
-            <div class="nameAndTimeDiv flex-col items-start justify-center">
-                <div class="flex gap-2 items-center justify-center">
-                    <p class="font-bold">${post.author.fullname}</p>
-                    <i class="fa-solid fa-check" style="color: #74C0FC;"></i>
+            postContainer.innerHTML = `
+            <div class="userDetails flex items-center gap-1 h-12">
+                <div class="profilePicDiv w-10 h-10 rounded-full overflow-hidden">
+                    <img class="w-10"
+                        src="https://media.licdn.com/dms/image/D4D03AQGG79xYxGe2uQ/profile-displayphoto-shrink_200_200/0/1694942905209?e=2147483647&v=beta&t=I5P_2oMiMWQcEk_A2KMTobYaZkrwhAyzKVWBs4cZcvc"
+                        alt="">
                 </div>
-                <p class="leading-none text-sm">
-                    11 min ago
-                </p>
-            </div>
-        </div> <!-- userDetails ends -->
+    
+                <div class="nameAndTimeDiv flex-col items-start justify-center">
+                    <div class="flex gap-2 items-center justify-center">
+                        <p class="font-bold">${post.author.fullname}</p>
+                        <i class="fa-solid fa-check" style="color: #74C0FC;"></i>
+                    </div>
+                    <p class="leading-none text-sm">
+                        11 min ago
+                    </p>
+                </div>
+            </div> <!-- userDetails ends -->
+    
+            <div class="postContent h-fit w-full mt-2">
+                <p>${post.content}</p>
+            </div> <!-- postContent ends -->
+    
+            <div class="postImage w-full h-[15rem] flex items-center justify-center">
+                <img class=" h-full"
+                    src="/media/uploads/${post.image}"
+                    alt="">
+            </div> <!-- postImage ends -->
+    
+            <div class="postInteractionsDiv flex gap-10 items-center justify-start mt-3">
+    
+                <div
+                    class="group likeDiv rounded-lg border-2 border-zinc-300 p-2 flex gap-1 items-center justify-center cursor-pointer">
+                    <i class="fa-regular fa-heart group-hover:text-red-500 text-blue-400"></i>
+                    <p>${post.likes.length}</p>
+                </div>
+    
+                <div
+                    class="commentDiv rounded-lg border-2 border-zinc-300 p-2  flex gap-1 items-center justify-center cursor-pointer">
+                    <i class="fa-regular fa-comment-dots text-green-500"></i>
+                    <p>${post.comments.length}</p>
+                </div>
+    
+                <div
+                    class="shareDiv rounded-lg border-2 border-zinc-300 p-2 flex gap-1 items-center justify-center cursor-pointer">
+                    <i class="fa-solid fa-share text-orange-400"></i>
+                    <p>Share</p>
+                </div>
+            </div> <!-- postInteractions ends -->`
 
-        <div class="postContent h-fit w-full mt-2">
-            <p>${post.content}</p>
-        </div> <!-- postContent ends -->
-
-        <div class="postImage w-full h-[15rem] flex items-center justify-center">
-            <img class=" h-full"
-                src="/media/uploads/${post.image}"
-                alt="">
-        </div> <!-- postImage ends -->
-
-        <div class="postInteractionsDiv flex gap-10 items-center justify-start mt-3">
-
-            <div
-                class="group likeDiv rounded-lg border-2 border-zinc-300 p-2 flex gap-1 items-center justify-center cursor-pointer">
-                <i class="fa-regular fa-heart group-hover:text-red-500 text-blue-400"></i>
-                <p>${post.likes.length}</p>
-            </div>
-
-            <div
-                class="commentDiv rounded-lg border-2 border-zinc-300 p-2  flex gap-1 items-center justify-center cursor-pointer">
-                <i class="fa-regular fa-comment-dots text-green-500"></i>
-                <p>${post.comments.length}</p>
-            </div>
-
-            <div
-                class="shareDiv rounded-lg border-2 border-zinc-300 p-2 flex gap-1 items-center justify-center cursor-pointer">
-                <i class="fa-solid fa-share text-orange-400"></i>
-                <p>Share</p>
-            </div>
-        </div> <!-- postInteractions ends -->`
-        
-        allPostsContainer.appendChild(postContainer);
-    });
+            allPostsContainer.appendChild(postContainer);
+        });
+    }
 });
 
 
