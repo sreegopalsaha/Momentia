f1a1ab320ba238336ecc8fdc241108c177adaebc
Sree Gopal Saha <sreegopal0101@gmail.com>
2024-08-19 16:50:57 +0530

    fix field name

--- api/getFeed.js
+++ api/getFeed.js
@@ -3,11 +3,11 @@ const userModel = require("../models/user.model");
 
 const getFeed = async (req, res) => {
 
-    const user = await userModel.findById(req.user.id).populate('following');
-    if(user.following.length<1 && user.posts.length<1 ){
+    const user = await userModel.findById(req.user.id).populate('followings');
+    if(user.followings.length<1 && user.posts.length<1 ){
         return res.json("");
     }
-    const followingUserIds = user.following.map(followingUser => followingUser._id);
+    const followingUserIds = user.followings.map(followingUser => followingUser._id);
     followingUserIds.push(user._id);
     const followingPosts = await postModel.find({ author: { $in: followingUserIds } })
     .populate("author", "fullname username profilePicture")
